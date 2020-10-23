efea686e610908cb62201440ea8f983280b24e27
Brian McClain <brianmmcclain@gmail.com>
2020-10-23 16:51:40 -0400

    Add zipkin tracing

--- backend/src/main/java/com/github/brianmmcclain/springzipkindemo/WebController.java
+++ backend/src/main/java/com/github/brianmmcclain/springzipkindemo/WebController.java
@@ -1,9 +1,11 @@
 package com.github.brianmmcclain.springzipkindemo;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@EnableAutoConfiguration
 public class WebController {
     
     @GetMapping("/")
--- frontend/src/main/java/com/github/brianmmcclain/springzipkindemo/WebController.java
+++ frontend/src/main/java/com/github/brianmmcclain/springzipkindemo/WebController.java
@@ -3,14 +3,25 @@ package com.github.brianmmcclain.springzipkindemo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 
 @RestController
+@EnableAutoConfiguration
 public class WebController {
+
+    @Autowired 
+    RestTemplate restTemplate;
     
     @GetMapping("/")
     public String helloWorld() {
-        RestTemplate restTemplate = new RestTemplate();
         String response = restTemplate.getForObject("http://localhost:8082", String.class);
         return "<h1>Hello from the " + response + "!</h1>";
+    }
+
+    @Bean 
+    RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
